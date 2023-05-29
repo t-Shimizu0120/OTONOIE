98e809636fd52a325f4a05eed7130cf93d04d9d0
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-05-29 13:15:44 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-
+const add_tabContents = document.createElement('div');
+add_tabContents.setAttribute('class','js-added-tab-contents');
+for (let i = 0; i < 3; i++) {
+  const tab_Input = document.createElement('input');
+  const setId = 'tab' + (i + 1);
+  tab_Input.setAttribute('id',setId);
+  tab_Input.setAttribute('type','radio');
+  tab_Input.setAttribute('name','name');
+  if (i === 0) {
+    tab_Input.setAttribute('checked','');
+  };
+  
+  const tab_Label = document.createElement('label');
+  tab_Label.setAttribute('class','label');
+  tab_Label.setAttribute('for',setId);
+  
+  add_tabContents.appendChild(tab_Input);
+  add_tabContents.appendChild(tab_Label);
+};
+console.log(add_tabContents);
