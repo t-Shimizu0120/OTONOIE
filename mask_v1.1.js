5c8a5217b55e71415cd3715e64ec578056bc6d77
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-06-02 12:10:37 +0900

    Update mask_v1.1.js

--- mask_v1.1.js
+++ mask_v1.1.js
@@ -426,7 +426,7 @@ if (locationURL.includes('detail')) {
             {title:'qrcode'},
             {alt:'qrcode'}
         ]);
-        qr_Img.src = `https://api.qrserver.com/v1/create-qr-code/?data=${detailPageUrl}&amp;size=50x50&amp;margin=3`; 
+        qr_Img.src = `https://api.qrserver.com/v1/create-qr-code/?data=${detailPageUrl}&size=100x100&margin=3`; 
         qr_div.appendChild(qr_Img);
         return qr_div;
     })();
