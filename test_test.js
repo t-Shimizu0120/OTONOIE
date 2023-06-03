bca39ca1a95dfed52bfe4824e98ef334c6db5788
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-06-03 22:09:35 +0900

    Update test_test.js

--- test_test.js
+++ test_test.js
@@ -22,22 +22,29 @@
             {class:'js-added-share-label'},
             {for:'id-share'}
         ]);
-        const share_img = create_Element('img',[
+        const share_Img = create_Element('img',[
             {src:'https://www.otonoie.net/otonoie/img/prep/p8238/83560b3f27ae7909e9859b63855b95af.png'},
             {alt:'share-img'},
             {class:'share-img'}
         ]);
-        const share_img_close = create_Element('img',[
+        const share_Img_Close = create_Element('img',[
             {src:'https://www.otonoie.net/otonoie/img/prep/p8238/83560b3f27ae7909e9859b63855b95af.png'},
             {alt:'share-img-close'},
             {class:'share-img-close'}
         ]);
-        share_Label.appendChild(share_img);
-        share_Label.appendChild(share_img_close);
+        share_Label.appendChild(share_Img);
+        share_Label.appendChild(share_Img_Close);
         share_Div.appendChild(share_Input);
         share_Div.appendChild(share_Label);
+        
+        const share_Style = create_Element('style',[
+            {id:'https://www.otonoie.net/otonoie/img/prep/p8238/83560b3f27ae7909e9859b63855b95af.png'},
+            {alt:'share-img'},
+            {class:'share-img'}
+        ]);
+        
         return share_Div;
     })();
-    const insertTargetElm = document.querySelector('div.detail_btm');
+    const shareTargetElm = document.querySelector('#pagetop');
     insertTargetElm.appendChild(share);
     
