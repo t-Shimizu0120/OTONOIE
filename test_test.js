018ef50eabd4ba4a287345f48de596488fc5ab53
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-06-04 10:49:54 +0900

    Update test_test.js

--- test_test.js
+++ test_test.js
@@ -77,3 +77,24 @@
         })();
         insertTargetElm.before(share);
     })();
+
+
+
+
+window.onload=function(){
+    let Animation = function() {
+        //アイコン位置取得
+        let pageTop =  document.getElementById('share');
+        //要素の位置座標を取得
+        let rect = pageTop.getBoundingClientRect();
+        //topからの距離
+        let scrollTop = rect.top + window.pageYOffset;
+ 
+        if (scrollTop > 420) {
+            pageTop.classList.add('show');
+        }  else {
+            pageTop.classList.remove('show');
+        };
+    };
+    window.addEventListener('scroll', Animation);
+};
