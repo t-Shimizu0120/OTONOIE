1a19a8c2d29c6f69b48fe42ca88df71811fa4b03
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-06-07 16:56:10 +0900

    Update mask_v1.1.js

--- mask_v1.1.js
+++ mask_v1.1.js
@@ -428,7 +428,7 @@ if (locationURL.includes('detail')) {
                 {
                     contents_Title:'',
                     table_BaseId:'qrcode',
-                    table_Contents:[[{th:'携帯表示用QRコード'},{td:[]}]],
+                    table_Contents:[[{th:'<p>携帯表示用</p><p>QRコード</p>'},{td:[]}]],
                     add_Styles:`
                     `, 
                     add_To_Selector:''
