78cf11cf00ec996b045adec158e81614cb5a8f79
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-06-10 21:48:28 +0900

    Update mask_v1.1.js

--- mask_v1.1.js
+++ mask_v1.1.js
@@ -598,6 +598,7 @@ if (locationURL.includes('detail')) {
             #share-1 {
                 display:block !important;
                 width: 25% !important;
+                margin-bottom:8px;
             }
             
             .js-added-line {
