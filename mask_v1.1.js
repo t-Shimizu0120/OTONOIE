0a79b485cadcf5dc8991c62adfc1c31a88c98b6d
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-07-04 02:29:00 +0900

    Update mask_v1.1.js

--- mask_v1.1.js
+++ mask_v1.1.js
@@ -1185,8 +1185,9 @@ if (locationURL.includes('detail')) {
         inquiry_wrap.querySelector('#month').id += '-invalid';
         inquiry_wrap.querySelector('textarea').id += 'textarea-invalid';
         inquiry_Bottom_Buttons_wrap.appendChild(inquiry_wrap.querySelector('ul.owner_form_btns'));
-        inquiry_Bottom_Buttons_wrap.appendChild(popover_Close_Button);
+        //inquiry_Bottom_Buttons_wrap.appendChild(popover_Close_Button);
         popover_Content_div.appendChild(inquiry_wrap);
+        popover_Content_div.appendChild(popover_Close_Button);
         inquiry_wrap.querySelector('.form-table.owner-form').appendChild(handlingOfPersonalInformation.cloneNode(true));
         inquiry_wrap.querySelector('form').appendChild(inquiry_Bottom_Buttons_wrap);
         //const inquiry_Bottom_Buttons_wrap_client_h = inquiry_Bottom_Buttons_wrap.clientHeight;
