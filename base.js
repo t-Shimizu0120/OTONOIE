35bdf693edbe92cbf285b5a039121bece4fe2e1b
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-07-06 19:28:18 +0900

    Update base.js

--- base.js
+++ base.js
@@ -1,16 +1,20 @@
-//=================================================================
-//pagetopボタン
-//=================================================================
-(() => {
-    const pageTopElm = document.getElementById('pagetop');
-    const pageTop_a = pageTopElm.querySelector('a');
-    pageTop_a.removeChild(pageTop_a.querySelector('img'));
-    pageTop_a.setAttribute('class','js-added-pagetop');
-    const pageTop_div = create_Element('div',[
-        {class:'js-added-pagetop-arrow'}
-    ]);
-    const pageTop_p = create_Element('p',[]);
-    pageTop_p.textContent = 'ページTOP';
-    pageTop_a.appendChild(pageTop_div);
-    pageTop_a.appendChild(pageTop_p);
-})();
+if (settings['mask']) {
+    //=================================================================
+    //pagetopボタン
+    //=================================================================
+    (() => {
+        const pageTopElm = document.getElementById('pagetop');
+        const pageTop_a = pageTopElm.querySelector('a');
+        pageTop_a.removeChild(pageTop_a.querySelector('img'));
+        pageTop_a.setAttribute('class','js-added-pagetop');
+        const pageTop_div = create_Element('div',[
+            {class:'js-added-pagetop-arrow'}
+        ]);
+        const pageTop_p = create_Element('p',[]);
+        pageTop_p.textContent = 'ページTOP';
+        pageTop_a.appendChild(pageTop_div);
+        pageTop_a.appendChild(pageTop_p);
+    })();
+} else {
+    
+};
