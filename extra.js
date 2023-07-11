34ad6ca3e84e32b7e1a41182508a649fefc88254
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-07-11 16:07:58 +0900

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,4 +1,16 @@
 if (settings['mask']) {
-    
+    //URL取得
+    const locationURL = location.href;
+    //URLで分岐
+    if (locationURL.includes('detail')) {
+        //=================================================================
+        //既存Style変更（Detailページ）
+        //=================================================================
+        const headElm = document.querySelector('head');
+        const bodyElm = document.querySelector('body');
+       
+    } else if (locationURL.includes('property')) {
+    } else {
+    };
 } else {
 };
