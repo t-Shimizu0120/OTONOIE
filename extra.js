bc0ee540c794f3e38fae734ea15fb65e335ac49c
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-07-13 16:02:19 +0900

    Update extra.js

--- extra.js
+++ extra.js
@@ -15,6 +15,54 @@ if (settings['mask']) {
         //管理費
         //問い合わせフォーム
         //個人情報の取り扱い
+        (() => {
+            //=================================================================
+            //コンテナ
+            //=================================================================
+            //----------------------------上部---------------------------------
+            const containerTop = create_Element('div',[
+                {class:'js-added-container-top'},
+                {id:'container-top'}
+            ]);
+            document.querySelector('div.detail_l').appendChild(containerTop);
+            const containerTop_Left = create_Element('div',[
+                {class:'js-added-container-top-left'},
+                {id:'container-top-left'}
+            ]);
+            containerTop.appendChild(containerTop_Left);
+            const containerTop_Right = create_Element('div',[
+                {class:'js-added-container-top-right'},
+                {id:'container-top-right'}
+            ]);
+            containerTop.appendChild(containerTop_Right);
+            const containerTop_Bottom = create_Element('div',[
+                {class:'js-added-container-top-bottom'},
+                {id:'container-top-bottom'}
+            ]);
+            containerTop.appendChild(containerTop_Bottom);
+            //-----------------------------------------------------------------
+            //----------------------------中部---------------------------------
+            const containerMiddle = create_Element('div',[
+                {class:'js-added-container-middle'},
+                {id:'container-middle'}
+            ]);
+            document.querySelector('div.detail_r').appendChild(containerMiddle);
+            //-----------------------------------------------------------------
+            //----------------------------下部---------------------------------
+            const containerBottom = create_Element('div',[
+                {class:'js-added-container-bottom'},
+                {id:'container-bottom'}
+            ]);
+            document.querySelector('div.detail_btm').appendChild(containerBottom);
+            //-----------------------------------------------------------------
+            //----------------------------シェア-------------------------------
+            const containerShare = create_Element('div',[
+                {class:'js-added-container-share'},
+                {id:'container-share'}
+            ]);
+            document.querySelector('div.detail_btm').appendChild(share_container);
+            //-----------------------------------------------------------------
+        })();
     } else if (locationURL.includes('property')) {
     } else {
     };
