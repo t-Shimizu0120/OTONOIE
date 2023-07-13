05cb099f919c51e6709a31487d2f340e440dfac3
t-Shimizu0120 <134357790+t-Shimizu0120@users.noreply.github.com>
2023-07-13 21:23:56 +0900

    Update extra.js

--- extra.js
+++ extra.js
@@ -21,43 +21,43 @@ if (settings['mask']) {
             //=================================================================
             //----------------------------上部---------------------------------
             const containerTop = create_Element('div',[
-                {class:'js-added-container-top'},
+                {class:'js-added-container-top component--flex-box'},
                 {id:'container-top'}
             ]);
             document.querySelector('div.detail_l').appendChild(containerTop);
             const containerTop_Left = create_Element('div',[
-                {class:'js-added-container-top-left'},
+                {class:'js-added-container-top-left component--flex-box'},
                 {id:'container-top-left'}
             ]);
             containerTop.appendChild(containerTop_Left);
             const containerTop_Right = create_Element('div',[
-                {class:'js-added-container-top-right'},
+                {class:'js-added-container-top-right component--flex-box'},
                 {id:'container-top-right'}
             ]);
             containerTop.appendChild(containerTop_Right);
             const containerTop_Bottom = create_Element('div',[
-                {class:'js-added-container-top-bottom'},
+                {class:'js-added-container-top-bottom component--flex-box'},
                 {id:'container-top-bottom'}
             ]);
             containerTop.appendChild(containerTop_Bottom);
             //-----------------------------------------------------------------
             //----------------------------中部---------------------------------
             const containerMiddle = create_Element('div',[
-                {class:'js-added-container-middle'},
+                {class:'js-added-container-middle component--flex-box'},
                 {id:'container-middle'}
             ]);
             document.querySelector('div.detail_r').appendChild(containerMiddle);
             //-----------------------------------------------------------------
             //----------------------------下部---------------------------------
             const containerBottom = create_Element('div',[
-                {class:'js-added-container-bottom'},
+                {class:'js-added-container-bottom component--flex-box'},
                 {id:'container-bottom'}
             ]);
             document.querySelector('div.detail_btm').appendChild(containerBottom);
             //-----------------------------------------------------------------
             //----------------------------シェア-------------------------------
             const containerShare = create_Element('div',[
-                {class:'js-added-container-share'},
+                {class:'js-added-container-share component--flex-box'},
                 {id:'container-share'}
             ]);
             document.querySelector('div.detail_btm').appendChild(containerShare);
